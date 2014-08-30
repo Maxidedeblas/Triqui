46cd8fc7902b7b8fb58bb8f0e09a019144f523d9
Maxidedeblas <xxmaxidexx@hotmail.com>
2014-08-30 17:03:33 -0500

    logica 2

--- hh.js
+++ hh.js
@@ -1,6 +1,6 @@
-
-var arr1 = [0,0,0];
-var arr2 = [0,0,1];
+var x = 1
+var arr1 = [1,0,1];
+var arr2 = [0,1,1];
 var arr3 = [1,0,0];
 var turno = 1 
 var x 
@@ -13,26 +13,49 @@ var circulo ={
 	juegos : 4,
 }
 
-while(equis.puntos > 0 && circulo.puntos > 0) {
-	if(turno ==1){
-		if (((arr1[0] == 0) && (arr1[1] == 0) && (arr1[2] == 0)) || 
-			((arr2[0] == 0) && (arr2[1] == 0) && (arr2[2] == 0)) ||
-	 		 ((arr3[0] == 0) && (arr3[1] == 0) && (arr3[2] == 0))) {
-				alert("triqui por fila gana o");
-		}else if (((arr1[0] == 0) && (arr2[0] == 0) && (arr3[0] == 0)) || 
-		  		  ((arr1[1] == 0) && (arr2[1] == 0) && (arr3[1] == 0)) ||
-	              ((arr1[2] == 0) && (arr2[2] == 0) && (arr3[2] == 0)))
-			  {
-					console.log(arr1);
-					alert("triqui por colubna gana o");
-			  }else if (((arr1[0]==0) && (arr2[1]==0) && (arr3[2]==0)) ||
-			          	((arr1[2]==0) && (arr2[1]==0) && (arr3[0]==0))) {
-						alert(" triqui por diagonal gana o");
-					}
-				else {
-					turno = 2;
-					alert("marrano !!!");
+while(equis.juegos > 0 && circulo.juegos > 0) {
+if(turno ==1){
+//Condiciones queevaluan que esten tres campos seguidos para ganar		
+if (((arr1[0] == 0) && (arr1[1] == 0) && (arr1[2] == 0)) || 
+	((arr2[0] == 0) && (arr2[1] == 0) && (arr2[2] == 0)) ||
+	 ((arr3[0] == 0) && (arr3[1] == 0) && (arr3[2] == 0))) {
+	alert("triqui por fila");
+	equis.juegos = 0 ;
+}else if (((arr1[0] == 0) && (arr2[0] == 0) && (arr3[0] == 0)) || 
+		  ((arr1[1] == 0) && (arr2[1] == 0) && (arr3[1] == 0)) ||
+	       ((arr1[2] == 0) && (arr2[2] == 0) && (arr3[2] == 0)))
+			{
+				equis.juegos = 0 ;
+				alert("triqui por colubna");
+			}else if (((arr1[0]==0) && (arr2[1]==0) && (arr3[2]==0)) ||
+			          ((arr1[2]==0) && (arr2[1]==0) && (arr3[0]==0))) {
+				alert(" triqui por diagonal");
+				equis.juegos = 0 ;
 			}
-	};		
-	
+			else{
+				equis.juegos = equis.juegos - 1 ;
+				turno=2;
+		
+}
+}
+		if (((arr1[0] == 1) && (arr1[1] == 1) && (arr1[2] == 1)) || 
+					((arr2[0] == 1) && (arr2[1] == 1) && (arr2[2] == 1)) ||
+					 ((arr3[0] == 1) && (arr3[1] == 1) && (arr3[2] == 1))) {
+					 	circulo.juegos = 0;
+					alert("triqui por fila gana la x");
+				}else if (((arr1[0] == 1) && (arr2[0] == 1) && (arr3[0] == 1)) || 
+						  ((arr1[1] == 1) && (arr2[1] == 1) && (arr3[1] == 1)) ||
+					       ((arr1[2] == 1) && (arr2[2] == 1) && (arr3[2] == 1)))
+							{
+								circulo.juegos = 0;
+								alert("triqui por colubna gana la x");
+							}else if (((arr1[0]==1) && (arr2[1]==1) && (arr3[2]==1)) ||
+							          ((arr1[2]==1) && (arr2[1]==1) && (arr3[0]==1))) {
+								circulo.juegos = 0;
+								alert(" triqui por diagonal gana la x");
+							}
+							else {
+								circulo.juegos = circulo.juegos - 1;
+								alert("marrano  x!!!");
+							}
 }
